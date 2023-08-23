4dbfdd180f2de13e7ef95d03ca5dfee98e83f7b8
mery0808 <142896087+mery0808@users.noreply.github.com>
2023-08-23 07:08:25 +0100

    Initial commit

---
+++ index.ts
@@ -0,0 +1,18 @@
+let mery1: Number;
+let mery2: number;
+let mery3: number;
+let mery4: number;
+let mery5: number;
+mery1 = 40;
+mery2 = 40;
+mery3 = 57;
+mery4 = 31;
+
+mery5 = mery1 - mery2 - mery3 - mery4;
+console.log(mery5);
+
+
+
+//###########################           SSTech       ###################
+//###########################   Programming Courses  ###################
+//######################################################################
