00255454824b28d5db48b041fdecf2fcee25fba0
mery0808 <142896087+mery0808@users.noreply.github.com>
2023-08-23 07:09:25 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -1,17 +1,6 @@
-let mery1: Number;
-let mery2: number;
-let mery3: number;
-let mery4: number;
-let mery5: number;
-mery1 = 40;
-mery2 = 40;
-mery3 = 57;
-mery4 = 31;
-
-mery5 = mery1 - mery2 - mery3 - mery4;
-console.log(mery5);
-
-
+let texte : string:
+texte = "bonjour";
+console.log(bonjour);
 
 //###########################           SSTech       ###################
 //###########################   Programming Courses  ###################
